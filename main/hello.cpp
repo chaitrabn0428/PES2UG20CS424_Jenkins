08ec49a5126c92736c55b8dc1d30c90571fc7e29
chaitrabn0428 <114558602+chaitrabn0428@users.noreply.github.com>
2023-02-16 14:39:39 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
   cout << "PES2UG20CS424";
   return 0;
 }
